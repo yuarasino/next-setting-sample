c05159009e589fce7f9f539562b36d15a5cb1ac5
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2023-03-11 08:19:36 +0900

    :airplane: UIライブラリを追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { CssBaseline } from "@nextui-org/react"
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document"
+import React from "react"
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class _Document extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context)
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
